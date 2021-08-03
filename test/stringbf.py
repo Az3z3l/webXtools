97d63541fb963b80359b1cd877cf18358cafe0e4
Az3z3l <yaswant.yme@gmail.com>
2021-08-03 19:24:25 +0530

    update test.py

--- test/stringbf.py
+++ test/stringbf.py
@@ -1,17 +1,19 @@
 import webXtools
 import hashlib
 import string
+import time
 
-
-# Find a string such that `hashlib.sha256("string".encode('utf-8')).hexdigest()[:4]` returns `7c21`
+# Find a string such that `hashlib.sha256("string".encode('utf-8')).hexdigest()[:5]` returns `3f6ac`
 
 
 def check(string):
-    if hashlib.sha256(string.encode('utf-8')).hexdigest()[:4] == "7c21":
+    if hashlib.sha256(string.encode('utf-8')).hexdigest()[:5] == "3f6a4":
         return True
     else:
         return False
 
-value = webXtools.bruteforce(minLength=1, maxLength=4, charSet=string.ascii_letters+string.digits, noOfThreads=3, callback=check)
+start = time.time()
+print(webXtools.bruteforce(minLength=1, maxLength=4, charSet=string.ascii_letters+string.digits, noOfThreads=4, callback=check))
+end = time.time()
 
-print(value)
+print(f"Runtime of the program is {end - start}")
